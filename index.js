ecfc311bc26c2d4ed9c9830ea597861f68f4de70
Maksym Sharin <Maksym.sharin.work@gmail.com>
2020-08-31 23:15:14 +0200

    feat(index): add main code draft

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
+/*
 
+let taxPolicy  //import json with tax
+let inputString = ´""
+let basket = new Basket();
+let receipt = new Receipt();
+let taxHandler = new TaxHandler();
+
+//load taxes to apply from a policy file
+taxHandler.setTaxPolicy( taxPolicy );
+
+//Console Interface for Handling user input
+    //"insert Products /n"
+    //"Format: [n] [product name] at [price]"
+inputString = await consoleInterface.prompt();
+
+basket.parseProducts( inputString );
+basket.getProducts.forEach(
+    product => {
+        taxHandler.applyPolicies( product );
+        receipt.addRow(
+        `${product.quantity} `  +
+        `${product.name}: `     +
+        `${product.taxedPrice}`
+        );
+    }
+);
+receipt.addRow( `Sales Taxes: ${taxHandler.taxesApplied} `);
+receipt.addRow( `Total: ${taxHandler.totalGrossApplied} `);
+receipt.print();
+
+
+
+
+*/
 
 
 
