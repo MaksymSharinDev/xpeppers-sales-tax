f2970be66390716aacb8350fa8bf15f69021d700
Maksym Sharin <Maksym.sharin.work@gmail.com>
2020-09-01 05:44:13 +0200

    feat(consoleBasket): add consoleBasket Module

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
+var consoleBasket = require("./bin/consoleBasket.js");
 /*
-
 let taxPolicy  //import json with tax
-let inputString = ´""
 let basket = new Basket();
 let receipt = new Receipt();
 let taxHandler = new TaxHandler();
-
 //load taxes to apply from a policy file
 taxHandler.setTaxPolicy( taxPolicy );
+*/
 
 //Console Interface for Handling user input
     //"insert Products /n"
     //"Format: [n] [product name] at [price]"
-inputString = await consoleInterface.prompt();
-
-basket.parseProducts( inputString );
-basket.getProducts.forEach(
-    product => {
+consoleBasket.prompt(
+    inputString => {
+        console.log( inputString );
+        /*
+        basket.parseProducts( inputString );
+        basket.getProducts.forEach(
+        product => {
         taxHandler.applyPolicies( product );
         receipt.addRow(
         `${product.quantity} `  +
         `${product.name}: `     +
         `${product.taxedPrice}`
         );
+        }
+        );
+        receipt.addRow( `Sales Taxes: ${taxHandler.taxesApplied} `);
+        receipt.addRow( `Total: ${taxHandler.totalGrossApplied} `);
+        receipt.print();
+        */
     }
 );
-receipt.addRow( `Sales Taxes: ${taxHandler.taxesApplied} `);
-receipt.addRow( `Total: ${taxHandler.totalGrossApplied} `);
-receipt.print();
 
 
-
-
-*/
 
 
 
